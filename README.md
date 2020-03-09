86980570a018ae971010cde03415feb9143e5f50
Zobair <zobair.career@gmail.com>
2020-03-09 02:09:49 -0400

    auto commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Novel Coronavirus JSON data (Updated: 08 March, 2020  20:19:13) 
+# Novel Coronavirus JSON data (Updated: 09 March, 2020  02:01:59) 
 This repository tracks Novel Coronavirus data and converts it into a json to be consumed by other applications. 
 The data is updated form Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE) repository. 
 You can visit https://github.com/CSSEGISandData/COVID-19 for more information. 
@@ -10,18 +10,30 @@ This repository gets updated automatically.
 Please see https://www.jokecamp.com/blog/code-examples-api-http-get-json-different-languages/ on how to to fetch these data. 
 
 
+## Files:
+
+        csse_covid_19_daily_reports.json
+        csse_covid_19_daily_reports_pretty_print.json
+        csse_covid_19_data/
+        time_series_19-covid-Confirmed.json
+        time_series_19-covid-Confirmed_pretty_print.json
+        time_series_19-covid-Deaths.json
+        time_series_19-covid-Deaths_pretty_print.json
+        time_series_19-covid-Recovered.json
+        time_series_19-covid-Recovered_pretty_print.json
+
 ### URLs
 ```
-latest_pretty_print = "https://raw.githubusercontent.com/zmsp/coronavirus-json-api/master/latest_pretty_print.json"
 latest = "https://raw.githubusercontent.com/zmsp/coronavirus-json-api/master/latest.json"
 metadata = "https://raw.githubusercontent.com/zmsp/coronavirus-json-api/master/metadata.json"
 ```
+Note that each dataset has a pretty print and a condensed pair 
 
 
 
-## latest.json file
+## 
 Provides record in an a list format
-```
+```json
 [
   {
     "Confirmed":"6593",
@@ -50,7 +62,9 @@ Provides record in an a list format
 
 ## metadata file
 Metadata provides useful information about each columns. Intended to help with statistical analysis of the data.
-``
+
+
+```json
 {
   "columns_meta":{
     "Confirmed":{
@@ -90,11 +104,126 @@ Metadata provides useful information about each columns. Intended to help with s
       "alpha_3":"DZA",
       "name":"Algeria"
     }
-...and more
   },
   "data_meta":{
     "md5":"4b2c3d9fd532d48f68e7a822c6ef9bb7",
     "updated":"07 March, 2020  00:20:13"
   }
 }
-``
+
+```
+
+```json
+{
+ "US": {
+        "total": {
+            "Province/State": 106.0,
+            "Lat": 38.46143301886793,
+            "Long": -95.28658490566035,
+            "1/22/20": 1.0,
+            "1/23/20": 1.0,
+            "1/24/20": 2.0,
+            "1/25/20": 2.0,
+            "1/26/20": 5.0,
+            "1/27/20": 5.0,
+            "1/28/20": 5.0,
+            "1/29/20": 5.0,
+            "1/30/20": 5.0,
+            "1/31/20": 7.0,
+            "2/1/20": 8.0,
+            "2/2/20": 8.0,
+            "2/3/20": 11.0,
+            "2/4/20": 11.0,
+            "2/5/20": 12.0,
+            "2/6/20": 12.0,
+            "2/7/20": 12.0,
+            "2/8/20": 12.0,
+            "2/9/20": 12.0,
+            "2/10/20": 12.0,
+            "2/11/20": 13.0,
+            "2/12/20": 13.0,
+            "2/13/20": 15.0,
+            "2/14/20": 15.0,
+            "2/15/20": 15.0,
+            "2/16/20": 15.0,
+            "2/17/20": 15.0,
+            "2/18/20": 15.0,
+            "2/19/20": 15.0,
+            "2/20/20": 15.0,
+            "2/21/20": 35.0,
+            "2/22/20": 35.0,
+            "2/23/20": 35.0,
+            "2/24/20": 53.0,
+            "2/25/20": 53.0,
+            "2/26/20": 59.0,
+            "2/27/20": 60.0,
+            "2/28/20": 62.0,
+            "2/29/20": 70.0,
+            "3/1/20": 76.0,
+            "3/2/20": 101.0,
+            "3/3/20": 121.0,
+            "3/4/20": 152.0,
+            "3/5/20": 220.0,
+            "3/6/20": 277.0,
+            "3/7/20": 416.0,
+            "3/8/20": 538.0
+        },
+        "Province": [
+            {
+                "Province/State": "King County, WA",
+                "Country/Region": "US",
+                "Lat": 47.6062,
+                "Long": -122.3321,
+                "1/22/20": 1,
+                "1/23/20": 1,
+                "1/24/20": 1,
+                "1/25/20": 1,
+                "1/26/20": 1,
+                "1/27/20": 1,
+                "1/28/20": 1,
+                "1/29/20": 1,
+                "1/30/20": 1,
+                "1/31/20": 1,
+                "2/1/20": 1,
+                "2/2/20": 1,
+                "2/3/20": 1,
+                "2/4/20": 1,
+                "2/5/20": 1,
+                "2/6/20": 1,
+                "2/7/20": 1,
+                "2/8/20": 1,
+                "2/9/20": 1,
+                "2/10/20": 1,
+                "2/11/20": 1,
+                "2/12/20": 1,
+                "2/13/20": 1,
+                "2/14/20": 1,
+                "2/15/20": 1,
+                "2/16/20": 1,
+                "2/17/20": 1,
+                "2/18/20": 1,
+                "2/19/20": 1,
+                "2/20/20": 1,
+                "2/21/20": 1,
+                "2/22/20": 1,
+                "2/23/20": 1,
+                "2/24/20": 1,
+                "2/25/20": 1,
+                "2/26/20": 1,
+                "2/27/20": 1,
+                "2/28/20": 1,
+                "2/29/20": 6,
+                "3/1/20": 9,
+                "3/2/20": 14,
+                "3/3/20": 21,
+                "3/4/20": 31,
+                "3/5/20": 51,
+                "3/6/20": 58,
+                "3/7/20": 71,
+                "3/8/20": 83
+            },
+            {... and more}
+]}
+}
+
+```
--- update_data.py
+++ update_data.py
@@ -7,17 +7,30 @@ import sys
 import numpy as np
 import pandas as pd
 import pycountry
-CSV_PATH = "./latest.csv"
+
 JSON_PRETTY_PATH = "./latest_pretty_print.json"
 JSON_PATH = "./latest.json"
-MD5_FILE_PATH = "./MD5.txt"
+
 README_PATH = "./README.md"
 META_FILE = "./metadata.json"
 META_PRETTY_FILE = "./metadata_pretty_print.json"
 
+DOWNLOAD_PATH = "./csse_covid_19_data/"
+DOWNLOAD_INFO_FILE = DOWNLOAD_PATH + "download_info.json"
+
+
+class npEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.int64):
+            return int(obj)
+        if isinstance(obj, np.int32):
+            return int(obj)
+        return json.JSONEncoder.default(self, obj)
+
+country_dict = []
+
 
 def dumpCsvToJson(CSV_FILE):
-
     with open(CSV_FILE, "r") as f:
         reader = csv.reader(f)
         headers = next(reader)
@@ -25,9 +38,11 @@ def dumpCsvToJson(CSV_FILE):
         out = json.dumps([row for row in reader])
     with open('latest.json', "w") as f:
         f.write(out)
+
+
 def dumpMetaData(CSV_FILE):
-    csvData=pd.read_csv(CSV_FILE, sep=',')
-    f=csvData.copy()
+    csvData = pd.read_csv(CSV_FILE, sep=',')
+    f = csvData.copy()
     f["Last Update"] = pd.to_datetime(f["Last Update"])
     placeholder = "null";
     data = {}
@@ -41,20 +56,20 @@ def dumpMetaData(CSV_FILE):
         total = ""
         unique = []
         if np.issubdtype(f[i].dtype, np.number):
-            max =f[i].max().__str__()
-            min =f[i].min().__str__()
+            max = f[i].max().__str__()
+            min = f[i].min().__str__()
             mean = f[i].mean().__str__()
-            median =f[i].median().__str__()
+            median = f[i].median().__str__()
             std = f[i].std().__str__()
-            total =  f[i].sum().__str__()
+            total = f[i].sum().__str__()
         elif np.issubdtype(f[i].dtype, np.datetime64):
-            max =f[i].max().__str__()
-            min =f[i].min().__str__()
+            max = f[i].max().__str__()
+            min = f[i].min().__str__()
             mean = f[i].mean().__str__()
         elif np.issubdtype(f[i].dtype, np.object):
             unique = f[i].fillna("NaN").unique().tolist()
 
-        data[i] = {"max":max, "min":min, "median":median, "mean":mean, "std":std, "unique": unique, "total":total}
+        data[i] = {"max": max, "min": min, "median": median, "mean": mean, "std": std, "unique": unique, "total": total}
 
     country_meta = {
     }
@@ -62,7 +77,8 @@ def dumpMetaData(CSV_FILE):
     for name in f["Country/Region"].unique():
         try:
             i = name
-            correction = {"Mainland China": "China", "South Korea": "Korea, Democratic", "Macao": "Macao"}
+            correction = {"Mainland China": "China", "South Korea": "Korea, Democratic", "Macao": "Macau",
+                          "UK": "United Kingdom", "Republic of Ireland": "Ireland"}
 
             if correction.__contains__(name):
                 country = pycountry.countries.search_fuzzy(correction[i])[0]
@@ -71,14 +87,14 @@ def dumpMetaData(CSV_FILE):
 
             country_meta[i] = {"name": country.name, "alpha_2": country.alpha_2, "alpha_3": country.alpha_3}
         except:
-            country_meta[i] = {"name":i, "alpha_2":"XX", "alpha_3":"XXX"}
-    md5 = md5Checksum(CSV_FILE,None)
+            country_meta[i] = {"name": i, "alpha_2": "XX", "alpha_3": "XXX"}
+    md5 = md5Checksum(CSV_FILE, None)
 
     meta_data = {
         "country_meta": country_meta,
         "columns_name": f.columns.unique().tolist(),
         "columns_meta": data,
-        "data_meta": {"md5":md5, "updated":datetime.datetime.now().strftime('%d %B, %Y  %H:%M:%S')}
+        "data_meta": {"md5": md5, "updated": datetime.datetime.now().strftime('%d %B, %Y  %H:%M:%S')}
     }
     out = json.dumps(meta_data)
     with open(META_FILE, "w") as f:
@@ -88,9 +104,14 @@ def dumpMetaData(CSV_FILE):
         f.write(out)
 
 
-
-
-
+def dumpTimeSeries(CSV_FILE):
+    with open(CSV_FILE, "r") as f:
+        reader = csv.reader(f)
+        headers = next(reader)
+        reader = csv.DictReader(f, headers)
+        out = json.dumps([row for row in reader], sort_keys=True, indent=2, separators=(',', ':'))
+    with open(JSON_PRETTY_PATH, "w") as f:
+        f.write(out)
 
 
 def dumpCsvToJsonPrettyPrint(CSV_FILE):
@@ -102,9 +123,10 @@ def dumpCsvToJsonPrettyPrint(CSV_FILE):
     with open(JSON_PRETTY_PATH, "w") as f:
         f.write(out)
 
-def md5Checksum(filePath,url):
+
+def md5Checksum(filePath, url):
     m = hashlib.md5()
-    if url==None:
+    if url == None:
         with open(filePath, 'rb') as fh:
             m = hashlib.md5()
             while True:
@@ -116,47 +138,170 @@ def md5Checksum(filePath,url):
     else:
         r = requests.get(url)
         for data in r.iter_content(8192):
-             m.update(data)
+            m.update(data)
         return m.hexdigest()
+
 
 def update_readme(first_line):
     with open(README_PATH) as f:
         lines = f.readlines()
-    lines[0] =first_line
+    lines[0] = first_line
     with open(README_PATH, "w") as f:
         f.writelines(lines)
 
-if __name__ == "__main__":
 
-
-    today = datetime.datetime.now()+datetime.timedelta(days=1)
+def get_data_url():
+    today = datetime.datetime.now() + datetime.timedelta(days=1)
     template_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{DATE}.csv"
     CSV_URL = template_URL.format(DATE=today.strftime('%m-%d-%Y'))
 
-    status_code =0
-    while( status_code != 200):
+    status_code = 0
+    while (status_code != 200):
         CSV_URL = template_URL.format(DATE=today.strftime('%m-%d-%Y'))
         r = requests.get(CSV_URL)
         status_code = r.status_code
         today = today - datetime.timedelta(days=1)
-    with open(CSV_PATH, 'wb') as f:
-        f.write(r.content)
+    return CSV_URL
 
-    with open(MD5_FILE_PATH, 'r') as file:
-        local_md5 = file.read().replace('\n', '')
 
-    latest_md5 = md5Checksum(CSV_PATH, None)
-    if (latest_md5 != local_md5):
-        dumpCsvToJson(CSV_PATH)
-        dumpCsvToJsonPrettyPrint(CSV_PATH)
-        dumpMetaData(CSV_PATH)
-        with open(MD5_FILE_PATH, 'w') as file:
-            file.write(latest_md5)
-        update_readme("# Novel Coronavirus JSON data (Updated: {UPDATED}) \n".format(UPDATED=datetime.datetime.now().strftime('%d %B, %Y  %H:%M:%S')))
-        print("updated")
+def dumpTimeSeries(CSV_FILE):
+    import pandas as pd
+    my_data = pd.read_csv(CSV_FILE)
+    agg = pd.DataFrame(columns=my_data.columns)
+    agg.loc[0] = "TOTAL"
+
+    f = {'Province/State': 'nunique',
+         'Lat': 'mean',
+         'Long': 'mean',
+         }
+
+    for i in my_data.columns[4:]:
+        agg[i].loc[0] = my_data[i].sum()
+        f[i] = "sum"
+
+    my_data = my_data.replace(np.nan, '', regex=True)
+    g = my_data.groupby(['Country/Region'])
+    aggra = g.agg(f)
+    results = {}
+    for key, df_gb in g:
+        results[str(key)] = {
+            "total": aggra.loc[str(key)].to_dict(),
+            "Province": df_gb.to_dict('records')
+        }
+    filename, ext = (CSV_FILE.split('/')[-1].split('.'))
+
+    filename = filename.replace("-","_").lower()
+    with open('./{}.json'.format(filename), "w") as f:
+        f.write(json.dumps(results))
+    with open('./{}_pretty_print.json'.format(filename), "w") as f:
+        f.write(json.dumps(results, indent=4))
+
+
+def download_data():
+    with open(DOWNLOAD_INFO_FILE, 'r') as file:
+        download_info = json.load(file)
+
+    URL_CONFIRMED = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    URL_DEATHS = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+    URL_RECOVERED = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+    URL_DAILY_REPORT = "generated/csse_covid_19_daily_reports.csv"
+    changed = []
+    data_changed = False;
+    for i in [URL_CONFIRMED, URL_DEATHS, URL_RECOVERED, URL_DAILY_REPORT]:
+        filename = i.split('/')[-1]
+        if (i == "generated/csse_covid_19_daily_reports.csv"):
+            i = get_data_url()
+        if filename in download_info:
+            old_md5 = download_info[filename]
+        else:
+            old_md5 = ""
+
+        curr_md5 = md5Checksum(None, i)
+        if (old_md5 != curr_md5):
+            r = requests.get(i)
+            with open(DOWNLOAD_PATH + filename, 'wb') as f:
+                f.write(r.content)
+                download_info[filename] = curr_md5
+                data_changed = True
+        changed.append(filename)
+
+    if (data_changed):
+        out = json.dumps(download_info, indent=4, sort_keys=True)
+        with open(DOWNLOAD_INFO_FILE, 'w') as f:
+            f.write(out)
+        print(out)
+    return changed
+
+
+
+def dumpRecordData(CSV_FILE):
+    import pandas as pd
+    my_data = pd.read_csv(CSV_FILE)
+
+    my_data["Last Update"] = pd.to_datetime(my_data["Last Update"])
+    agg = pd.DataFrame(columns=my_data.columns)
+    agg.loc[0] = "TOTAL"
+    agg.loc["Last Update"] = my_data["Last Update"].max()
+
+    operations = {'Province/State': 'nunique',
+         'Latitude': 'mean',
+         'Longitude': 'mean',
+         'Last Update': 'max'
+         }
+
+    for i in my_data.columns[3:]:
+        agg[i].loc[0] = my_data[i].sum()
+        operations[i] = "sum"
+    my_data["Last Update"] = my_data["Last Update"].dt.strftime('%Y-%m-%d %H:%M:%S')
+    my_data.loc[-1] =agg.loc[0]
+
+
+    my_data = my_data.replace(np.nan, '', regex=True)
+    # my_data["Confirmed"]= my_data["Confirmed"].astype(np.int32)
+    # my_data["Deaths"] =my_data["Deaths"].astype(np.int32)
+    # my_data["Recovered"] =my_data["Recovered"].astype(np.int32)
+
+
+
+    g = my_data.groupby(['Country/Region'])
+    aggra = g.agg(operations)
+
+    aggra["Province/State"] = aggra["Province/State"].astype(np.int32)
+    results = {}
+    for key, df_gb in g:
+        agg_dict = aggra.loc[str(key)].to_dict()
+        agg_dict["Province/State"] = "None"
+        results[str(key)] = {
+            "total": aggra.loc[str(key)].to_dict(),
+            "Province": df_gb.to_dict('records')
+        }
+    filename, ext = (CSV_FILE.split('/')[-1].split('.'))
+    filename = filename.replace("-","_").lower()
+    with open('./{}.json'.format(filename), "w") as file:
+        file.write(json.dumps(results, cls=npEncoder))
+    with open('./{}_pretty_print.json'.format(filename), "w") as file:
+        file.write(json.dumps(results, cls=npEncoder, indent=4, sort_keys=True))
+
+if __name__ == "__main__":
+
+    changed = download_data()
+
+    if (len(changed) != 0):
+        print("processing :" + changed.__str__())
+        for filename in changed:
+            CSV_PATH = DOWNLOAD_PATH + filename;
+            if filename == "csse_covid_19_daily_reports.csv":
+                dumpRecordData(CSV_PATH)
+                dumpCsvToJson(CSV_PATH)
+                dumpCsvToJsonPrettyPrint(CSV_PATH)
+                dumpMetaData(CSV_PATH)
+
+            else:
+                dumpTimeSeries(CSV_PATH)
+        update_readme("# Novel Coronavirus JSON data (Updated: {UPDATED}) \n".format(
+            UPDATED=datetime.datetime.now().strftime('%d %B, %Y  %H:%M:%S')))
+
+
+
     else:
         sys.exit('Local file is the latest, nothing was updated')
-
-
-
-
